b7bc863ae86fa50d02ebda23b319555131692455
MrKeyto <lopezcristian97david@gmail.com>
2022-03-14 18:59:38 -0300

    Agregue battletoad

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,25 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("./clampy.png");
+//const myLoader = new Loader();
+Loader.shared.add({url: "./battletoad.png", name: "myBattletoad"}) //Para llamar por referencia
+Loader.shared.add({url: "./clampy.png", name: "myClampy"})
 
-clampy.anchor.set(0.5);
 
-clampy.x = 300;
-clampy.y = 300;
+//function(){} //Esto es parecido al parentesis-flechita
+Loader.shared.onComplete.add(()=>{
 
-app.stage.addChild(clampy);
+	const clampy: Sprite = Sprite.from("myBattletoad");
+
+	console.log("Hola mundo! Ancho: ", clampy.width , " Alto: " , clampy.height);
+	
+	clampy.anchor.set(0);
+	
+	clampy.x = 0;
+	clampy.y = 0;
+	
+	app.stage.addChild(clampy);
+
+})
+
+Loader.shared.load();
